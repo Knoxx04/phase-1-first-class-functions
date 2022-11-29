c36cab32b279b7c722e9df0a474165a086f5907b
Knoxx04 <onyiknoxx3@gmail.com>
2022-11-29 13:04:26 +0300

    completed assignment

---
+++ index.js
@@ -0,0 +1,22 @@
+
+const receivesAFunction = function (callback) {
+  callback();
+};
+receivesAFunction();
+
+function returnsANamedFunction() {
+  return function namedFunction() {
+    console.log("I am the returned named function");
+  };
+}
+
+function returnsAnAnonymousFunction() {
+  return function () {
+    console.log("I am the returned anonymous function");
+  };
+}
+
+ 
+  
+  
+  
